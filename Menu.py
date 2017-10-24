bdf9efb0c99e661d4bb335854c02bd6f8ad055b8
Mary Tsymbaliuk <32141471+mashnyash@users.noreply.github.com>
2017-10-24 15:11:21 +0300

    Add files via upload

---
+++ Menu.py
@@ -0,0 +1,10 @@
+
+def My_Menu():
+    print("Show the entire database?          Press 1")
+    print("Add an item to the database?       Press 2")
+    print("Remove element from the database?  Press 3")
+    print("Find a book in the database?       Press 4")
+    print("My function?                       Press 5")
+    print("Fffilter?                          Press 6")
+    print("Exit from the program?             Press 0")
+    return input()
